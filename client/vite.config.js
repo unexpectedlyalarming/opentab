27bf7175b96daffd71a4d26f27d26760bad9f18a
antiblockchain <57572382+antiblockchain@users.noreply.github.com>
2023-12-21 08:55:45 -0600

    Gh pages setup... again

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,20 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { ViteGhPages } from "vite-plugin-gh-pages";
-
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/opentab/",
-  plugins: [
-    react(),
-    VitePWA(),
-    ViteGhPages({
-      // Options are optional
-      branch: "pages",
-      dir: "dist",
-      push: true,
-    }),
-  ],
+  plugins: [react()],
 });
